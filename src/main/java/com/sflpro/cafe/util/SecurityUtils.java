c7e4207a766304fec22902c56ea72435036c42d6
nnaltakyan <narek.naltakyan@dataart.com>
2021-12-30 14:07:55 +0400

    formatting

--- src/main/java/com/sflpro/cafe/util/SecurityUtils.java
+++ src/main/java/com/sflpro/cafe/util/SecurityUtils.java
@@ -1,13 +1,11 @@
 package com.sflpro.cafe.util;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
-import java.util.stream.Stream;
 
 public final class SecurityUtils
 {
